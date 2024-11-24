941810f1a22450b0741260fd6cc29a0b5f6f1966
YaidelAbreu <77790107+YaidelAbreu@users.noreply.github.com>
2024-11-24 09:05:58 -0300

    Include the route of notes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
-from app.routers import auth
+from app.routers import auth, notes
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/api/auth", tags=["Authentication"])
+app.include_router(notes.router, prefix="/api/notes", tags=["Notes"])
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the API"}
