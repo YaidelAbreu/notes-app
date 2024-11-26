944679b7193daadccdfb68dd6f41a7ffb1215c8e
YaidelAbreu <77790107+YaidelAbreu@users.noreply.github.com>
2024-11-26 17:16:17 -0300

    Add user types

--- frontend/src/slices/authSlice.ts
+++ frontend/src/slices/authSlice.ts
@@ -1,5 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+type User = {
+  email: string;
+  password: string;
+};
+
+type NewUser = User & {
+  name: string;
+};
+
+type UserBasicInfo = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+type UserProfileData = {
+  name: string;
+  email: string;
+};  
+
+type AuthApiState = {
+  basicUserInfo?: UserBasicInfo | null;
+  userProfileData?: UserProfileData | null;
+  status: "idle" | "loading" | "failed";
+  error: string | null;
+};
+
 const authSlice = createSlice({
   name: "auth",
   initialState: "",
