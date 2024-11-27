fe95b23046b216ca09226132d92a287e8581b57d
YaidelAbreu <77790107+YaidelAbreu@users.noreply.github.com>
2024-11-27 16:39:13 -0300

    Add env.example

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,4 +1,7 @@
+from dotenv import load_dotenv
 import os
+
+load_dotenv(".env.example")
 
 SECRET_KEY = os.getenv(
     "SECRET_KEY",
@@ -16,5 +19,6 @@ REFRESH_TOKEN_EXPIRE_MINUTES = 15 * 24 * 60  # 15 days
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "0.0.0.0")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 POSTGRES_USER = os.getenv("POSTGRES_USER", "user")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "user")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "user")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "app-notes")
+ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(",")
