a7477f8ae0b914cd3e966ba29193ebcd33ca12b4
YaidelAbreu <77790107+YaidelAbreu@users.noreply.github.com>
2024-11-27 08:08:57 -0300

    Update Home Page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,31 +1,16 @@
 import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks/redux-hooks";
-import { getUser, logout } from "../slices/authSlice";
-import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
 
   const basicUserInfo = useAppSelector((state) => state.auth.basicUserInfo);
 
-  useEffect(() => {
-    if (basicUserInfo) {
-      dispatch(getUser(basicUserInfo.id));
-    }
-  }, [basicUserInfo, dispatch]);
-
-  const handleLogout = async () => {
-    await dispatch(logout());
-    navigate("/login");
-  };
+  useEffect(() => {}, [basicUserInfo, dispatch]);
 
   return (
     <>
-      {/* <h1>Home</h1> */}
-      {/* <Button variant="contained" sx={{ mt: 3, mb: 2 }} onClick={handleLogout}>
-        Logout
-      </Button> */}
+      <h1>Home</h1>
     </>
   );
 };
