72f8d2a4fb69b7af6875123528c89365265a11e0
YaidelAbreu <77790107+YaidelAbreu@users.noreply.github.com>
2024-11-27 10:54:09 -0300

    Add FormModal template

---
+++ frontend/src/components/modal/FormModal.tsx
@@ -0,0 +1,51 @@
+import { Grid } from "@mui/material";
+import ModalPopup from "../modal/ModalPopup";
+import SubmitButton from "../forms/SubmitButton";
+import CancelButton from "../forms/CancelButton";
+import { closeModal } from "../../slices/modalSlice";
+import { useAppDispatch } from "../../hooks/redux-hooks";
+
+interface TaskPopupProps {
+  title: string;
+  onSubmit: () => void;
+  classNames?: string;
+  children: React.ReactNode;
+}
+
+const FormModal = ({
+  title,
+  onSubmit,
+  classNames,
+  children
+}: TaskPopupProps) => {
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = () => {
+    onSubmit();
+    dispatch(closeModal());
+    // handleClose();
+  };
+
+  const handleClose = () => {
+    dispatch(closeModal());
+  };
+
+  return (
+    <>
+      {/* <CssBaseline /> */}
+      <ModalPopup title={title} classNames={classNames}>
+        {children}
+        <Grid container justifyContent="space-between" mt={2}>
+          <Grid item xs={5}>
+            <SubmitButton onClick={handleSubmit} />
+          </Grid>
+          <Grid item xs={5}>
+            <CancelButton onClick={handleClose} />
+          </Grid>
+        </Grid>
+      </ModalPopup>
+    </>
+  );
+};
+
+export default FormModal;
